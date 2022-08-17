1c6e107d252a32703a5af016e547c758e6cd7281
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-08-17 08:15:35 +0300

    add base fragment

---
+++ app/src/main/java/com/dictionaryapp/base_classes/BaseFragment.kt
@@ -0,0 +1,31 @@
+package com.dictionaryapp.base_classes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+    abstract fun setup()
+
+    abstract val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> VB
+    private lateinit var _binding: VB
+    protected val binding: VB
+        get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = inflate(inflater, container, false)
+
+        setup()
+
+        return _binding.root
+    }
+
+
+}
