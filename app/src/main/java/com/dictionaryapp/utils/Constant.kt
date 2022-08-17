24feae6704784dd077a469ea283a76488a2e9038
Taha Saad <flutterdev.taha.saad@gmail.com>
2022-08-17 08:40:04 +0300

    adding constant class

---
+++ app/src/main/java/com/dictionaryapp/utils/Constant.kt
@@ -0,0 +1,13 @@
+package com.dictionaryapp.utils
+
+object Constant {
+    const val SCHEMA = "https"
+    const val HOST = "api.dictionaryapi.dev"
+    const val FIRST_PATH = "api"
+    const val SECOND_PATH = "v2"
+    const val THIRD_PATH = "entries"
+    const val FORTH_PATH = "en"
+
+
+    const val TAG = "debug"
+}
